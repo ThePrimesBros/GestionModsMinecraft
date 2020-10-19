b714130d9b3ae34005777db1af9fca4f0cee17ad
Raphael Bessonnier <bessonnier@info03-15.iut45.univ-orleans.fr>
2020-10-19 15:32:42 +0200

    modification sur les views et models

--- GestionModsMinecraft/mods/models.py
+++ GestionModsMinecraft/mods/models.py
@@ -2,7 +2,7 @@ from django.db import models# Create your models here.
 
 class Mods(models.Model):
 
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     creator = models.CharField(max_length=255, default="")   
     description = models.TextField(default="", blank=True, null=True)
     version = models.CharField(max_length=30, default="", blank=True, null=True)  
